315151291504115cccb66c098136aa59997d5410
Shannon McPherron <mcpherron@eva.mpg.de>
2019-08-17 15:33:08 +0200

    making windows release

--- E5/build_installs/windows/rebuild.bat
+++ E5/build_installs/windows/rebuild.bat
@@ -5,6 +5,6 @@ copy e5-original.spec e5.spec
 python -m PyInstaller e5.spec
 copy missing-dlls\*.* dist\e5
 copy ..\..\e5.kv dist\e5
-copy dist\e5 ..\..\installs\windows
-cd ..\..\installs\windows
+Compress-Archive dist\e5 ..\..\installs\windows\e5.zip
+cd dist\e5
 e5.exe
